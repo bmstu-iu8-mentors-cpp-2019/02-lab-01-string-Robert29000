7cdeeaf448a126b65485d374f78c38f769ffa2ef
Robert29000 <mayka29000@mail.ru>
2020-03-24 17:48:51 +0300

    Added source dir and Cmake modified

---
+++ sources/string.cpp
@@ -0,0 +1,212 @@
+//Copyright 2020 Robert
+
+#include <stdio.h>
+#include <iostream>
+#include <string.hpp>
+
+String::~String(){
+ delete[] Data;
+}
+
+String::String(){
+ Data = nullptr;
+}
+
+String::String(const String& rhs){
+ size_t length = rhs.Size();
+ Data = new char[length + 1];
+ Data[length] = '\0';
+ for (int i = 0; i < length; i++){
+  Data[i] = rhs[i];
+ }
+}
+
+String::String(const char* data){
+ size_t length = strlen(data);
+ Data = new char[length+1];
+ Data[length] = '\0';
+ for (size_t i = 0; i < length; i++){
+  Data[i] = data[i];
+ }
+}
+
+String& String::operator=(const String& rhs){
+ delete [] Data;
+ size_t length = rhs.Size();
+ Data = new char[length+1];
+ Data[length] = '\0';
+ if (&rhs != this){
+  for (int i = 0 ; i < length ; i++){
+   Data[i] = rhs.Data[i];
+  }
+ }
+ return *this;
+}
+
+String& String::operator+=(const String& rhs){
+ (*this) = (*this) + rhs;
+ return *this;
+}
+
+String& String::operator*=(unsigned int m){
+ (*this) = (*this) * m;
+ return *this;
+}
+
+bool String::operator==(const String& rhs) const{
+ for (size_t i = 0 ; i < Size() ;i++){
+  if (Data[i] != rhs[i]){
+   return false;
+  }
+ }
+ return true;
+}
+
+bool String::operator<(const String& rhs) const{
+ for (size_t i = 0 ; i < Size(); i++){
+  if (Data[i] < rhs[i]){
+   return true;
+  }else if (Data[i] == rhs[i]){
+   continue;
+  }else{
+   return false;
+  }
+ }
+ return false;
+}
+
+size_t String::Find(const String& substr) const{
+ size_t word_length = substr.Size();
+ size_t pos = -1;
+ char* temp = new char[word_length+1];
+ temp[word_length] = '\0';
+ for (size_t i = 0 ; i < Size() - word_length + 1 ; i++){
+  memcpy(temp, &Data[i], word_length);
+  temp[word_length] = '\0';
+  String tempString(temp);
+  if (tempString == substr){
+   pos = i;
+   return pos;
+  }
+ }
+ delete[] temp;
+ return pos;
+}
+
+void String::Replace(char oldSymbol, char newSymbol){
+ for (size_t i = 0 ; i < Size(); i++){
+  if (Data[i] == oldSymbol){
+   Data[i] = newSymbol;
+  }
+ }
+}
+
+size_t String::Size() const{
+ return strlen(Data);
+}
+
+bool String::Empty() const{
+ if (Size() > 0 ){
+  return true;
+ }else{
+  return false;
+ }
+}
+
+char String::operator[](size_t index) const{
+ return Data[index];
+}
+
+char& String::operator[](size_t index){
+ return Data[index];
+}
+
+void String::RTrim(char symbol){
+ int count = 0;
+ for (size_t i = Size() - 1; i >= 0; i--){
+  if (Data[i] == symbol){
+   count++;
+  }else{
+   break;
+  }
+ }
+ size_t newLen = Size() - count;
+ char *data = new char[newLen+1];
+ data[newLen] = '\0';
+ int hCount = 0;
+ for (size_t i = 0; i < Size(); i++){
+  if (Data[i] != symbol || hCount == count){
+   data[i] = Data[i];
+  }else{
+   hCount++;
+  }
+ }
+ delete[] Data;
+ Data = data;
+}
+
+void String::LTrim(char symbol){
+ int count = 0;
+ for (size_t i = 0; i < Size(); i++){
+  if (Data[i] == symbol){
+   count++;
+  }else{
+   break;
+  }
+ }
+ size_t newLen = Size() - count;
+ char *data = new char[newLen+1];
+ data[newLen] = '\0';
+ int hCount = 0;
+ for (size_t i = 0; i < Size(); i++){
+  if (Data[i] != symbol || hCount == count){
+   data[i-count] = Data[i];
+  }else{
+   hCount++;
+  }
+ }
+ delete[] Data;
+ Data = data;
+}
+
+void String::swap(String& oth){
+ String temp;
+ temp = *this;
+ (*this) = oth;
+ oth = temp;
+}
+
+String operator+(const String& a, const String& b){
+ size_t length = a.Size() + b.Size();
+ char *data = new char[length+1];
+ data[length] = '\0';
+ for (int i = 0; i < length; i++){
+  if (i < a.Size()){
+   data[i] = a[i];
+  }else{
+   data[i] = b[i - a.Size()];
+  }
+ }
+ String res(data);
+ return res;
+}
+
+String operator*(const String& a, unsigned int b){
+ size_t length = a.Size() * (b+1);
+ char* data = new char[length+1];
+ data[length] = '\0';
+ int counter = 0;
+ for (int i = 0; i <= b ; i++){
+  for (size_t i = 0; i < a.Size(); i++){
+   data[counter] = a[i];
+   counter++;
+  }
+ }
+ String res(data);
+ return data;
+}
+
+std::ostream& operator<<(std::ostream& out, const String& str){
+ out << str.Data;
+ return out;
+}
